88f6afdc60db3fedceed2140f5eace79990edac2
d2newlife <Gitlife9620>
2019-12-10 13:21:24 -0500

    Update

---
+++ src/main/java/com/duanegrey/dcuisvc/DcuisvcApplication.java
@@ -0,0 +1,13 @@
+package com.duanegrey.dcuisvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DcuisvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DcuisvcApplication.class, args);
+    }
+
+}
