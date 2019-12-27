00168f57cd9e771d1423f36826fe1eded2631fcc
d2newlife <Gitlife9620>
2019-12-27 00:55:49 -0500

    Update

--- src/main/java/com/duanegrey/dcuisvc/model/BalanceRowTemplate.java
+++ src/main/java/com/duanegrey/dcuisvc/model/BalanceRowTemplate.java
@@ -15,8 +15,17 @@ public class BalanceRowTemplate implements IRowTemplate {
         listRowDesc.add(new CRowDesc("Assets", CConst.ROWGROUP,"",0.00));
         listRowDesc.add(new CRowDesc("Cash And Cash Equivalents", CConst.ROWDATA,"currentcash",1000.00));
         listRowDesc.add(new CRowDesc("Short Term Investment", CConst.ROWDATA,"shortterminvestments",1000.00));
-        listRowDesc.add(new CRowDesc("Long Term Investments", CConst.ROWDATA,"longterminvestments",1000.00));
         listRowDesc.add(new CRowDesc("Net Receivables", CConst.ROWDATA,"receivables",1000.00));
+        listRowDesc.add(new CRowDesc("Inventory", CConst.ROWDATA,"inventory",1000.00));
+        listRowDesc.add(new CRowDesc("Other Current Assets", CConst.ROWDATA,"othercurrentassets",1000.00));
+        listRowDesc.add(new CRowDesc("Total Current Assets", CConst.ROWDATA,"currentassets",1000.00));
+        listRowDesc.add(new CRowDesc("Non-Current Assets", CConst.ROWGROUP,"",0.00));
+        listRowDesc.add(new CRowDesc("Property, Plant, ...", CConst.ROWDATA,"propertyplantequipment",1000.00));
+        listRowDesc.add(new CRowDesc("Long Term Investments", CConst.ROWDATA,"longterminvestments",1000.00));
+        listRowDesc.add(new CRowDesc("Goodwill", CConst.ROWDATA,"goodwill",1000.00));
+        listRowDesc.add(new CRowDesc("Intangible Assets", CConst.ROWDATA,"intangibleassets",1000.00));
+        listRowDesc.add(new CRowDesc("Other Assets", CConst.ROWDATA,"otherassets",1000.00));
+        listRowDesc.add(new CRowDesc("Total Assets", CConst.ROWDATA,"totalassets",1000.00));
     }
 
     @Override
