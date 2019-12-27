599faa23da151b73c735cb32405212ddba639720
d2newlife <Gitlife9620>
2019-12-27 17:04:47 -0500

    Update

--- src/main/java/com/duanegrey/dcuisvc/model/BalanceRowTemplate.java
+++ src/main/java/com/duanegrey/dcuisvc/model/BalanceRowTemplate.java
@@ -26,6 +26,17 @@ public class BalanceRowTemplate implements IRowTemplate {
         listRowDesc.add(new CRowDesc("Intangible Assets", CConst.ROWDATA,"intangibleassets",1000.00));
         listRowDesc.add(new CRowDesc("Other Assets", CConst.ROWDATA,"otherassets",1000.00));
         listRowDesc.add(new CRowDesc("Total Assets", CConst.ROWDATA,"totalassets",1000.00));
+        listRowDesc.add(new CRowDesc("Liabilities", CConst.ROWGROUP,"",0.00));
+        listRowDesc.add(new CRowDesc("Accounts Payable", CConst.ROWDATA,"accountspayable",1000.00));
+        listRowDesc.add(new CRowDesc("Other Current Liabilities", CConst.ROWDATA,"othercurrentliabilities",1000.00));
+        listRowDesc.add(new CRowDesc("Total Current Liabilities", CConst.ROWDATA,"totalcurrentliabilities",1000.00));
+        listRowDesc.add(new CRowDesc("Long Term Debt", CConst.ROWDATA,"longtermdebt",1000.00));
+        listRowDesc.add(new CRowDesc("Total Liabilities", CConst.ROWDATA,"totalliabilities",1000.00));
+        listRowDesc.add(new CRowDesc("Stockholders' Equity", CConst.ROWGROUP,"",0.00));
+        listRowDesc.add(new CRowDesc("Common Stock", CConst.ROWDATA,"commonstock",1000.00));
+        listRowDesc.add(new CRowDesc("Retained Earnings", CConst.ROWDATA,"retainedearnings",1000.00));
+        listRowDesc.add(new CRowDesc("Capital Surplus", CConst.ROWDATA,"capitalsurplus",1000.00));
+        listRowDesc.add(new CRowDesc("Total Stockholders' Equity", CConst.ROWDATA,"shareholderequity",1000.00));
     }
 
     @Override
--- src/main/java/com/duanegrey/dcuisvc/model/IncomeRowTemplate.java
+++ src/main/java/com/duanegrey/dcuisvc/model/IncomeRowTemplate.java
@@ -19,6 +19,14 @@ public class IncomeRowTemplate implements IRowTemplate {
         listRowDesc.add(new CRowDesc("Research and Development", CConst.ROWDATA,"researchanddevelopment",1000.00));
         listRowDesc.add(new CRowDesc("Selling General and Administrative", CConst.ROWDATA,"sellinggeneralandadmin",1000.00));
         listRowDesc.add(new CRowDesc("Total Operating Expense", CConst.ROWDATA,"operatingexpense",1000.00));
+
+        listRowDesc.add(new CRowDesc("Operating Income / Loss", CConst.ROWDATA,"operatingincome",1000.00));
+        listRowDesc.add(new CRowDesc("Other Income/Expenses Net", CConst.ROWDATA,"otherincomeexpensenet",1000.00));
+        listRowDesc.add(new CRowDesc("Interest Expense On Debt", CConst.ROWDATA,"interestincome",1000.00));
+        listRowDesc.add(new CRowDesc("Pre-Tax Income", CConst.ROWDATA,"pretaxincome",1000.00));
+        listRowDesc.add(new CRowDesc("Income Tax Expenses", CConst.ROWDATA,"incometax",1000.00));
+        listRowDesc.add(new CRowDesc("Net Income", CConst.ROWDATA,"netincome",1000.00));
+        listRowDesc.add(new CRowDesc("EBIT", CConst.ROWDATA,"ebit",1000.00));
     }
 
     @Override
