3904336234ca5a76102479320fae61d667616618
d2newlife <Gitlife9620>
2019-12-27 20:10:00 -0500

    Update

--- src/main/java/com/duanegrey/dcuisvc/model/CashRowTemplate.java
+++ src/main/java/com/duanegrey/dcuisvc/model/CashRowTemplate.java
@@ -13,6 +13,15 @@ public class CashRowTemplate implements IRowTemplate {
         listRowDesc = new ArrayList<>();
         listRowDesc.add(new CRowDesc("Net Income", CConst.ROWDATA,"netincome",1000.00));
         listRowDesc.add(new CRowDesc("Depreciation and Amortization", CConst.ROWDATA,"depreciation",1000.00));
+        listRowDesc.add(new CRowDesc("Changes In Inventories", CConst.ROWDATA,"changesininventories",1000.00));
+        listRowDesc.add(new CRowDesc("Operating Cash Flow", CConst.ROWDATA,"cashflow",1000.00));
+        listRowDesc.add(new CRowDesc("Total Capital Expenditures", CConst.ROWDATA,"capitalexpenditures",1000.00));
+        listRowDesc.add(new CRowDesc("Purchase / Sale Investments", CConst.ROWDATA,"investments",1000.00));
+        listRowDesc.add(new CRowDesc("Other Investing Activities", CConst.ROWDATA,"investingactivityother",1000.00));
+        listRowDesc.add(new CRowDesc("Net Cash From Investing ..", CConst.ROWDATA,"totalinvestingcashflows",1000.00));
+        listRowDesc.add(new CRowDesc("Dividends Paid", CConst.ROWDATA,"dividendspaid",1000.00));
+        listRowDesc.add(new CRowDesc("Other Financing Activities", CConst.ROWDATA,"otherfinancingcashflows",1000.00));
+        listRowDesc.add(new CRowDesc("Net Cash From Financing ...", CConst.ROWDATA,"cashflowfinancing",1000.00));
     }
 
     @Override
